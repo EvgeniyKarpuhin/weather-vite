08e1c3102783f560df302504323540dee7aa6923
EvgeniyKarpuhin <81818834+EvgeniyKarpuhin@users.noreply.github.com>
2025-05-22 15:34:47 +0300

    add

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/weather-vite/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
